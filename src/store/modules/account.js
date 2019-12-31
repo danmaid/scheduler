0f8ec0d5ff7e55b81b717d9920dfce698ba8fe91
yamada@cockroachg.work <yamada@cockroachg.work>
2020-01-01 04:00:21 +0900

    ゴミ掃除

--- src/store/modules/account.js
+++ src/store/modules/account.js
@@ -13,34 +13,9 @@ const msalConfig = {
 export default {
   namespaced: true,
   state: {
-    msalInstance: new Msal.UserAgentApplication(msalConfig),
-    me: undefined,
-    calendars: undefined,
-    calendarGroups: undefined,
-    calendarGroupCalendars: undefined,
-    groups: undefined,
-    groupsCalendars: undefined
+    msalInstance: new Msal.UserAgentApplication(msalConfig)
   },
-  mutations: {
-    setMe(state, payload) {
-      state.me = payload
-    },
-    setCalendars(state, payload) {
-      state.calendars = payload
-    },
-    setCalendarGroups(state, payload) {
-      state.calendarGroups = payload
-    },
-    setCalendarGroupCalendars(state, payload) {
-      state.calendarGroupCalendars = payload
-    },
-    setGroups(state, payload) {
-      state.groups = payload
-    },
-    setGroupsCalendars(state, payload) {
-      state.groupsCalendars = payload
-    }
-  },
+  mutations: {},
   getters: {},
   actions: {
     async login({ state }) {
@@ -60,73 +35,6 @@ export default {
           return res.accessToken
         }
       }
-    },
-    async getMe({ commit, dispatch }) {
-      const accessToken = await dispatch('getAccessToken')
-      const headers = new Headers()
-      headers.append('Authorization', 'Bearer ' + accessToken)
-      const res = await fetch('https://graph.microsoft.com/v1.0/me', { headers })
-      logger.debug(res)
-      const data = await res.json()
-      commit('setMe', data)
-    },
-    async getCalendars({ commit, dispatch }) {
-      const accessToken = await dispatch('getAccessToken')
-      const headers = new Headers()
-      headers.append('Authorization', 'Bearer ' + accessToken)
-      const res = await fetch('https://graph.microsoft.com/v1.0/me/calendars', { headers })
-      logger.debug(res)
-      const data = await res.json()
-      commit('setCalendars', data)
-    },
-    async getCalendarGroups({ commit, dispatch }) {
-      const accessToken = await dispatch('getAccessToken')
-      const headers = new Headers()
-      headers.append('Authorization', 'Bearer ' + accessToken)
-      const res = await fetch('https://graph.microsoft.com/v1.0/me/calendarGroups', { headers })
-      logger.debug(res)
-      const data = await res.json()
-      commit('setCalendarGroups', data)
-    },
-    async getCalendarGroupCalendars({ commit, dispatch }) {
-      const accessToken = await dispatch('getAccessToken')
-      const headers = new Headers()
-      headers.append('Authorization', 'Bearer ' + accessToken)
-      const res = await fetch('https://graph.microsoft.com/v1.0/me/calendarGroups', { headers })
-      logger.debug(res)
-      const groups = await res.json()
-      for (let group of groups.value) {
-        const res = await fetch(`https://graph.microsoft.com/v1.0/me/calendarGroups/${group.id}/calendars`, { headers })
-        logger.debug(res)
-        const data = await res.json()
-        logger.debug(data)
-      }
-    },
-    async getGroups({ commit, dispatch }) {
-      const accessToken = await dispatch('getAccessToken')
-      const headers = new Headers()
-      headers.append('Authorization', 'Bearer ' + accessToken)
-      const res = await fetch('https://graph.microsoft.com/v1.0/groups', { headers })
-      logger.debug(res)
-      const data = await res.json()
-      commit('setGroups', data)
-    },
-    async getGroupsCalendars({ commit, dispatch }) {
-      const accessToken = await dispatch('getAccessToken')
-      const headers = new Headers()
-      headers.append('Authorization', 'Bearer ' + accessToken)
-      const res = await fetch('https://graph.microsoft.com/v1.0/groups', { headers })
-      logger.debug(res)
-      const data = await res.json()
-      const calendars = []
-      for (let group of data.value) {
-        const res = await fetch(`https://graph.microsoft.com/v1.0/groups/${group.id}/calendar`, { headers })
-        logger.debug(res)
-        const data = await res.json()
-        logger.debug(data)
-        calendars.push(data)
-      }
-      commit('setGroupsCalendars', calendars)
     }
   }
 }
